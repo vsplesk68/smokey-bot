a8357e7b7f75ec74dc5cff8df723ae810f68c13f
vsplesk68 <71484660+vsplesk68@users.noreply.github.com>
2020-10-18 14:50:39 +0300

    3

--- components/__init__.py
+++
@@ -1,9 +0,0 @@
-import components.commands
-import components.inlinequeries
-import components.messagehandlers
-
-
-def register(dp):
-    components.commands.register(dp)
-    components.inlinequeries.register(dp)
-    components.messagehandlers.register(dp)
